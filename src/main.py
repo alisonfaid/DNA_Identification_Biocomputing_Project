2a17265bd9948fafb06e1d9d686ac3044ca9794b
Alison Faid <afaid01@student.bbk.ac.uk>
2023-03-19 14:03:40 +0000

    test draft and final output working

--- src/main.py
+++ src/main.py
@@ -2,16 +2,16 @@ import seq_analysis as sa
 from Bio import SeqIO
 from Bio import Align
 
-mystery = 'data/mystery.fa'
-dog_breeds = 'data/dog_breeds.fa'
-dog_breeds_2 = 'data/dog_breeds.fa'
+mystery_file = 'data/mystery.fa'
+dog_breeds_file = 'data/dog_breeds.fa'
+dog_breeds_2_file = 'data/dog_breeds.fa'
 
-mystery = SeqIO.read(mystery, 'fasta')
-dog_breeds = SeqIO.parse(dog_breeds, 'fasta')
-dog_breeds_2 = SeqIO.parse(dog_breeds_2, 'fasta')
+mystery = SeqIO.read(mystery_file, 'fasta')
+dog_breeds = SeqIO.parse(dog_breeds_file, 'fasta')
+dog_breeds_2 = SeqIO.parse(dog_breeds_2_file, 'fasta')
 
 alignments = sa.alignment(mystery, dog_breeds)
 sequence_lengths = sa.seq_lens(dog_breeds_2)
-percentage_id = sa.p_identity(alignments, sequence_lengths)
-final = final_output(percentage_identity, dog_breeds_file)
+percentage_id = sa.percent_id(alignments, sequence_lengths)
+final = sa.final_output(percentage_id, dog_breeds_file)
 print(final)
--- src/seq_analysis.py
+++ src/seq_analysis.py
@@ -1,10 +1,17 @@
 from Bio import SeqIO
 from Bio import Align
 
+
 def alignment(seq, database):
-## Creates an alignment of the target sequence to each sequence in the database
-## Calculates a score for the number of matches between the target sequence and the database 
-## Stores the scores in a list 
+    """
+    - Input: an unknown sequence in fasta format, and a database of known sequence as a fasta iterator
+    
+    - Creates alignments of the target sequence to each sequence in the database
+    - Calculates a score for the number of aligned amino acids between the target sequence and the database 
+    - Stores the scores in a list 
+    
+    - Output: a list of alignment scores 
+    """
     aligner = Align.PairwiseAligner(match_score = 1.0)
     alignments = []
     for i in database:
@@ -17,13 +24,12 @@ def seq_lens(database):
 ## Calculates the length of each sequence in the reference database and stores them in a list
     lengths = []
     for i in database:
-        print(i)
         seq_length = len(i)
         lengths.append(seq_length)
     return lengths
 
 
-def p_identity(alignment_scores, db_lengths):
+def percent_id(alignment_scores, db_lengths):
     ## Calculates the percentage identity of the target to each sequence in the database
     ## Takes the alignent scores from the alignment function and the sequence lengths from the 
     ## seq_lens function
@@ -41,11 +47,15 @@ def final_output(p_id_list, database_file):
     for i, record in enumerate(SeqIO.parse(database_file, "fasta")):
         if i == max_index: 
             description = record.description
-            return description, record.seq
+            split = description.split(']')
+            breed = split[6]
+            breed = breed[8:]
+            new_line = '\n'
+            return f'Closest matching dog breed:{breed}{new_line}% difference:{percentage_difference}{new_line}sequence of matching breed:{record.seq}'
 
 
-
-
+database = 'data/dog_breeds.fa'
+print(final_output([99, 50, 25], database))
 
 
             
