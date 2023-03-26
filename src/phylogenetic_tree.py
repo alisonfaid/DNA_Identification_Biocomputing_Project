02c245921309070e27060924623b3d9138bd65f8
Alison Faid <afaid01@student.bbk.ac.uk>
2023-03-26 15:24:23 +0100

    phylogenetic tree - first working tree

--- src/phylogenetic_tree.py
+++ src/phylogenetic_tree.py
@@ -1,21 +1,34 @@
-from Bio import SeqIO
 from Bio import AlignIO
 from Bio import Phylo
-from Bio.Align.Applications import MafftCommandline
+from Bio.Phylo.TreeConstruction import DistanceCalculator
+from Bio.Phylo.TreeConstruction import DistanceTreeConstructor
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+mystery_file = 'data/mystery.fa'
+dog_breeds_file = 'data/dog_breeds.fa'
 
 
-mystery_file = r'data/mystery.fa'
-dog_breeds_file = r'data/dog_breeds.fa'
+alignment = AlignIO.read(dog_breeds_file, 'fasta')
 
-in_file = dog_breeds_file
-mafft_cline = MafftCommandline(input=in_file)
-print(mafft_cline)
-stdout, stderr = mafft_cline()
-with open("aligned.fasta", "w") as handle:
-    handle.write(stdout)
-align = AlignIO.read("aligned.fasta", "fasta")
 
-        
+calculator = DistanceCalculator('identity')
+distance_matrix = calculator.get_distance(alignment)
+
+
+constructor = DistanceTreeConstructor(calculator)
+tree = constructor.build_tree(alignment)
+
+
+
+fig = plt.figure(figsize=(13, 20), dpi=100) # create figure & set the size 
+plt.rc('font', size=12)             # fontsize of the leaf and node labels 
+plt.rc('xtick', labelsize=10)       # fontsize of the tick labels
+plt.rc('ytick', labelsize=10)       # fontsize of the tick labels
+#turtle_tree.ladderize()		   # optional way to reformat your tree 
+axes = fig.add_subplot(1, 1, 1)
+Phylo.draw(tree, axes=axes)
+#fig.savefig("turtles_cladogram")
     
 
 
