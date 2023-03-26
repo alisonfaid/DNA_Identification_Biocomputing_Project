e2d84edcce1a0f6e848ecbf2efddfeb64e0dc0ed
Alison Faid <afaid01@student.bbk.ac.uk>
2023-03-26 18:16:27 +0100

    mystery_seq added to alignment

--- src/phylogenetic_tree.py
+++ src/phylogenetic_tree.py
@@ -1,4 +1,7 @@
 from Bio import AlignIO
+from Bio import SeqIO
+from Bio import Seq
+from Bio.SeqRecord import SeqRecord
 from Bio import Phylo
 from Bio.Phylo.TreeConstruction import DistanceCalculator
 from Bio.Phylo.TreeConstruction import DistanceTreeConstructor
@@ -8,27 +11,41 @@ import matplotlib as mpl
 mystery_file = 'data/mystery.fa'
 dog_breeds_file = 'data/dog_breeds.fa'
 
-
 alignment = AlignIO.read(dog_breeds_file, 'fasta')
+mystery_seq = SeqIO.read(mystery_file, 'fasta')
+alignment.append(mystery_seq)
+
+def phylogenetic_tree(alignment_file):
+    """
+    Creates phylogenetic tree from alignment file and plots it using matplotlib
+
+    Args:
+        alignment_file (_type_): _description_
+    """
+    # Create distance calculator 
+    calculator = DistanceCalculator('identity')
+    distance_matrix = calculator.get_distance(alignment)
+    
+    # Create phylogenetic tree
+    constructor = DistanceTreeConstructor(calculator)
+    tree = constructor.build_tree(alignment)
+    
+    # Plot tree
+    fig = plt.figure(figsize=(15, 7.5), dpi=100) 
+    plt.rc('font', size=6)             
+    plt.rc('xtick', labelsize=10)       
+    plt.rc('ytick', labelsize=10)    
+    plt.rc('axes', titlesize=14) 
+    tree.ladderize()		   
+    axes = fig.add_subplot(1, 1, 1)
+    
+    return Phylo.draw(tree, axes=axes)
 
 
-calculator = DistanceCalculator('identity')
-distance_matrix = calculator.get_distance(alignment)
+
+phylogenetic_tree(alignment)
 
 
-constructor = DistanceTreeConstructor(calculator)
-tree = constructor.build_tree(alignment)
-
-
-
-fig = plt.figure(figsize=(13, 20), dpi=100) # create figure & set the size 
-plt.rc('font', size=12)             # fontsize of the leaf and node labels 
-plt.rc('xtick', labelsize=10)       # fontsize of the tick labels
-plt.rc('ytick', labelsize=10)       # fontsize of the tick labels
-#turtle_tree.ladderize()		   # optional way to reformat your tree 
-axes = fig.add_subplot(1, 1, 1)
-Phylo.draw(tree, axes=axes)
-#fig.savefig("turtles_cladogram")
     
 
 
