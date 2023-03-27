8b08f8d06c6d5ab1431851f682652e176c13119f
Alison Faid <afaid01@student.bbk.ac.uk>
2023-03-27 15:05:23 +0100

    Probabilities module added to main function

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 import dna_identification as id
+import probabilities as p
 
 mystery = 'data/mystery.fa'
 dog_breeds = 'data/dog_breeds.fa'
 
-def run_dna_identification(target_seq, database):
+
+def run_all_modules(target_seq, database):
     """
     Summary:
     - Finds the closest matching sequence in the database to the target sequence using 
@@ -18,11 +20,20 @@ def run_dna_identification(target_seq, database):
         string: ID, breed name, percentage difference and sequence for the closest matching
             sequence to the target in the database
     """
+    # dna_identification module
     alignments = id.alignment(target_seq, database)
     sequence_lengths = id.seq_lens(database)
     percentage_id = id.percent_id(alignments, sequence_lengths)
-    final_output = id.final_output(percentage_id, database)
-    return final_output
+    dna_id = id.final_output(percentage_id, database)
+    
+    # probabilities module
+    z_scores = p.z_score(alignments)
+    p_values = p.p_value(z_scores)
+    p_value_output = f"P-Values Across Database:{p_values}"
+    
+    
+    return dna_id, p_value_output
 
 
+# Run dna_identification tool 
 print(run_dna_identification(mystery, dog_breeds))
