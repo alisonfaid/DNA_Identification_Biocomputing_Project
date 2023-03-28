f6a82644636db7fbd7eca4b2ae7c3fefe95cfe43
Alison Faid <afaid01@student.bbk.ac.uk>
2023-03-28 10:45:09 +0100

    read files function added

--- src/main.py
+++ src/main.py
@@ -7,8 +7,11 @@ dog_breeds = 'data/dog_breeds.fa'
 
 
 # DNA identification analysis 
-alignments = id.alignment(mystery, dog_breeds)
-sequence_lengths = id.seq_lens(dog_breeds)
+target_seq = id.read_target_file(mystery)
+database = id.parse_database_file(dog_breeds)
+alignments = id.alignment(target_seq, database)
+database_2 = id.parse_database_file(dog_breeds)
+sequence_lengths = id.seq_lens(database_2)
 percentage_id = id.percent_id(alignments, sequence_lengths)
 dna_id = id.final_output(percentage_id, dog_breeds)
   
